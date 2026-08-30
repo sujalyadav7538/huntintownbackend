cea273e78ed31b2a151557a67283d5b3a3ce01e0
Sujal Yadav <sujalyadav7538@gmail.com>
2026-08-30 13:19:26 +0530

    Images Proper Validation

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,6 +1,7 @@
 // routes/postRoute.js
 
 import express from "express";
+import multer from "multer";
 import {
   createPost,
   getAllPosts,
@@ -14,8 +15,37 @@ import { verifyToken } from "../middlewares/authMiddleware.js";
 import { uploadPostImages } from "../middlewares/uploadMiddleware.js";
 
 const router = express.Router();
+const MAX_POST_IMAGES = 4;
 
-router.post("/", verifyToken, uploadPostImages.array("images", 3), createPost);
+router.post("/", verifyToken, (req, res, next) => {
+  uploadPostImages.array("images", MAX_POST_IMAGES)(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      if (err.code === "LIMIT_FILE_COUNT") {
+        return res.status(400).json({
+          success: false,
+          statusCode: 400,
+          message: `You can upload up to ${MAX_POST_IMAGES} images only.`,
+        });
+      }
+
+      if (err.code === "LIMIT_UNEXPECTED_FILE") {
+        return res.status(400).json({
+          success: false,
+          statusCode: 400,
+          message: "Unexpected upload field. Use the 'images' field.",
+        });
+      }
+
+      return next(err);
+    }
+
+    if (err) {
+      return next(err);
+    }
+
+    return createPost(req, res, next);
+  });
+});
 
 router.get("/", getAllPosts);
 router.get("/getAvailablePosts", verifyToken, getAvailablePosts);
